3d67238a20ccf4df1fd98ce9ebe8f1319f821b58
Aidan Murphy <aidan.murphy@mymail.champlain.edu>
2019-01-17 15:13:15 -0500

    adding week1-debug

---
+++ Week1-debug/Assets/FollowMouse_Error4.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowMouse_Error4 : MonoBehaviour {
+
+	GameObject player;
+
+	void start(){
+		player = GameObject.Find ("Player");
+	}
+
+	void Update () {
+		Follow ();
+	}
+
+
+	void Follow(){
+		Vector3 mousePosition = Input.mousePosition;//Get the mouse position in screen pixels
+		//convert screen coordinates to world space
+		Vector3 adjustedPosition = Camera.main.ScreenToWorldPoint (mousePosition);
+		//set the z of the player to 0
+		adjustedPosition.z = 0f;
+		//reposition the player
+		player.transform.position = adjustedPosition;
+	}
+}
