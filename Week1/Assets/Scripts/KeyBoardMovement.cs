5c56b6ff0905c021fcbdbeed294cfa16a136a215
Aidan Murphy <aidan.murphy@mymail.champlain.edu>
2019-01-31 14:10:22 -0500

    Finishing up week1

--- Week1/Assets/Scripts/KeyBoardMovement.cs
+++ Week1/Assets/Scripts/KeyBoardMovement.cs
@@ -16,7 +16,7 @@ public class KeyBoardMovement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         checkInput();
         MovePlayer();
