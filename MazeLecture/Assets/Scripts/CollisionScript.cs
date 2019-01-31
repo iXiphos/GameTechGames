5f803e68a5481df109f503ad32739b55f1fb03fa
Aidan Murphy <aidan.murphy@mymail.champlain.edu>
2019-01-31 15:08:19 -0500

    adding MazeLecture

---
+++ MazeLecture/Assets/Scripts/CollisionScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionScript : MonoBehaviour
+{
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("OnCollisionEnter2D");
+    }
+
+    void OnCollisionExit2D(Collision2D other)
+    {
+        Debug.Log("OnCollisionExit2D");
+    }
+
+    void OnCollisionStay2D(Collision2D other)
+    {
+        Debug.Log("OnCollisionStay2D");
+    }
+
+}
---
+++ MazeLecture/Assets/Scripts/TriggerScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerScript : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("OnTriggerEnter2D");
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log("OnTriggerExit2D");
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        Debug.Log("OnTriggerStay2D");
+    }
+
+}
