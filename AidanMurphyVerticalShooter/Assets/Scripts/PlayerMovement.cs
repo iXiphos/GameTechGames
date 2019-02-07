16887c1a26caa75686158b435695fcb63b55ff70
Aidan Murphy <aidan.murphy@mymail.champlain.edu>
2019-02-07 15:15:57 -0500

    Creating Vertical Shooter Movement and Basic Shooting

---
+++ AidanMurphyVerticalShooter/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float xSpeed;
+    float xMove = 0;
+    public float ySpeed;
+    float yMove = 0;
+    float boundsLeft = -5f;
+    float boundsRight = 5f;
+    float boundsUp = 5f;
+    float boundsDown = -8.5f;
+
+    Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckInput();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+        CheckBounds();
+    }
+
+    void Move()
+    {
+        Vector2 newVelocity = new Vector2(xMove, yMove);
+        rb2d.velocity = newVelocity;
+    }
+
+    void CheckBounds()
+    {
+        Vector2 maxPosX;
+        Vector2 maxPosY;
+        if(transform.position.x < boundsLeft)
+        {
+            maxPosX = new Vector2(boundsLeft, transform.position.y);
+            transform.position = maxPosX;
+        }
+        else if(transform.position.x > boundsRight)
+        {
+            maxPosX = new Vector2(boundsRight, transform.position.y);
+            transform.position = maxPosX;
+        }
+
+        if(transform.position.y < boundsDown)
+        {
+            maxPosY = new Vector2(transform.position.x, boundsDown);
+            transform.position = maxPosY;
+        }
+        else if (transform.position.y > boundsUp)
+        {
+            maxPosY = new Vector2(transform.position.x, boundsUp);
+            transform.position = maxPosY;
+        }
+    }
+
+    void CheckInput()
+    {
+        xMove = Input.GetAxis("Horizontal") * xSpeed;
+        yMove = Input.GetAxis("Vertical") * ySpeed;
+    }
+}
---
+++ AidanMurphyVerticalShooter/Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum firePattern {
+    straight,
+    shotGun
+}
+
+public class PlayerShooting : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+
+    Transform bulletSpawn;
+
+    firePattern firingPattern;
+
+    public float attackRate = 0;
+    private float timeToNextAttack = 0;
+
+    public float bulletSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        firingPattern = firePattern.straight;
+        bulletSpawn = GameObject.Find("BulletSpawn").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > timeToNextAttack)
+        {
+            switch (firingPattern)
+            {
+                case firePattern.straight:
+                    FireStraight();
+                    break;
+                case firePattern.shotGun:
+                    FireShotGun();
+                    break;
+            }
+        }
+    }
+
+    void FireStraight()
+    {
+        timeToNextAttack = Time.time + attackRate;
+        GameObject bullet = Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+        bullet.GetComponent<Rigidbody2D>().AddForce(transform.right * bulletSpeed);
+        Destroy(bullet, 3f);
+    }
+
+    void FireShotGun()
+    {
+
+    }
+
+}
