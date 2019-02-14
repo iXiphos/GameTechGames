f7b3e8d76bb1f0f7fc70ae1dacf2e48707a13bb1
Aidan Murphy <aidan.murphy@mymail.champlain.edu>
2019-02-13 19:04:51 -0500

    Adding firing upgrade, and enemy destruction

--- AidanMurphyVerticalShooter/Assets/Scripts/PlayerShooting.cs
+++ AidanMurphyVerticalShooter/Assets/Scripts/PlayerShooting.cs
@@ -13,7 +13,8 @@ public class PlayerShooting : MonoBehaviour
 
     Transform bulletSpawn;
 
-    firePattern firingPattern;
+    [HideInInspector]
+    public firePattern firingPattern;
 
     public float attackRate = 0;
     private float timeToNextAttack = 0;
@@ -30,7 +31,7 @@ public class PlayerShooting : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && Time.time > timeToNextAttack)
+        if (Input.GetKey(KeyCode.Space) && Time.time > timeToNextAttack)
         {
             switch (firingPattern)
             {
@@ -48,13 +49,22 @@ public class PlayerShooting : MonoBehaviour
     {
         timeToNextAttack = Time.time + attackRate;
         GameObject bullet = Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
-        bullet.GetComponent<Rigidbody2D>().AddForce(transform.right * bulletSpeed);
+        bullet.GetComponent<Rigidbody2D>().AddForce(transform.up * bulletSpeed);
         Destroy(bullet, 3f);
     }
 
     void FireShotGun()
     {
-
+        timeToNextAttack = Time.time + attackRate;
+        GameObject bullet = Instantiate(bulletPrefab, bulletSpawn.position, Quaternion.Euler(0,0,20));
+        GameObject bullet1 = Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+        GameObject bullet2 = Instantiate(bulletPrefab, bulletSpawn.position, Quaternion.Euler(0, 0, -20));
+        bullet.GetComponent<Rigidbody2D>().AddRelativeForce(transform.up * bulletSpeed);
+        bullet1.GetComponent<Rigidbody2D>().AddRelativeForce(transform.up * bulletSpeed);
+        bullet2.GetComponent<Rigidbody2D>().AddRelativeForce(transform.up * bulletSpeed);
+        Destroy(bullet, 3f);
+        Destroy(bullet1, 3f);
+        Destroy(bullet2, 3f);
     }
 
 }
