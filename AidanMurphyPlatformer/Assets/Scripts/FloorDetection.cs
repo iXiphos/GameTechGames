545cc4ca5bc81c8ee12a2f83b6d6e19f44524aa9
Aidan Murphy <aidan.murphy@mymail.champlain.edu>
2019-02-21 15:03:19 -0500

    Creating Platformer Project and starting movement

---
+++ AidanMurphyPlatformer/Assets/Scripts/FloorDetection.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorDetection : MonoBehaviour
+{
+    public bool touchingFloor = false;
+
+    LayerMask layer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        layer = LayerMask.GetMask("Ground");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Physics2D.OverlapBox(transform.position, Vector2.one, 0, layer)) touchingFloor = true;
+        else touchingFloor = false;
+    }
+}
---
+++ AidanMurphyPlatformer/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public GameObject groundCollider;
+
+    Rigidbody2D rgbd;
+
+    float defaultGravity;
+
+    public float moveSpeed;
+    public float jumpSpeed;
+
+    private bool grounded;
+
+    public float gravityIncreaseAmount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rgbd = gameObject.GetComponent<Rigidbody2D>();
+        defaultGravity = rgbd.gravityScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        grounded = groundCollider.GetComponent<FloorDetection>().touchingFloor;
+        Movement();
+        Jump();
+    }
+
+    void Movement()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        rgbd.velocity = new Vector2(moveX * moveSpeed, rgbd.velocity.y);
+    }
+
+    void Jump()
+    {
+        if(Input.GetButtonDown("Jump") && grounded)
+        {
+            rgbd.AddForce(Vector2.up * jumpSpeed);
+        }
+
+        if(rgbd.velocity.y < 0)
+        {
+            rgbd.gravityScale = defaultGravity * gravityIncreaseAmount * Time.deltaTime;
+        }else if(rgbd.velocity.y == 0)
+        {
+            rgbd.gravityScale = defaultGravity;
+        }
+
+    }
+
+}
