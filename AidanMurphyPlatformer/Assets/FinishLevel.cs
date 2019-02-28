991fe5c65e6aab42ee34f49bb85d2eb1946503a1
Aidan Murphy <aidan.murphy@mymail.champlain.edu>
2019-02-27 23:04:39 -0500

    Adding End Flag and Creating after image effect

---
+++ AidanMurphyPlatformer/Assets/FinishLevel.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class FinishLevel : MonoBehaviour
+{
+    public Text beatLevelText;
+
+    bool reloading = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            beatLevelText.enabled = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            beatLevelText.enabled = false;
+            reloading = false;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            reloading = true;
+        }
+    }
+
+    void Update()
+    {
+        if (reloading && Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("Reloading");
+            Scene scene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(scene.name);
+        }
+    }
+
+}
