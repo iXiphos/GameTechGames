053b1c7e0de1780883ec04b85e7dbab47248bb9b
Aidan Murphy <aidan.murphy@mymail.champlain.edu>
2019-04-17 13:59:51 -0400

    Adding Folders, bouncy ball that creates pulses

---
+++ FinalProject/GameTechFinalProject/Assets/Scripts/BloomEffect.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[ExecuteInEditMode, ImageEffectAllowedInSceneView]
+public class BloomEffect : MonoBehaviour
+{
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination);
+    }
+}
---
+++ FinalProject/GameTechFinalProject/Assets/Scripts/pulse.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pulse : MonoBehaviour
+{
+    public GameObject pulseEffect;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Memes");
+        GameObject pulseParticle = Instantiate(pulseEffect, transform.position, transform.rotation);
+    }
+}
