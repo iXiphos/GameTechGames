15503cf917a833e8ae5939d1de332e31addca80c
Aidan Murphy <aidan.murphy@mymail.champlain.edu>
2019-04-24 23:00:45 -0400

    Adding shooting

---
+++ FinalProject/GameTechFinalProject/Assets/Scripts/DefaultFire.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using StateStuff;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DefaultFire : State<PlayerShooting>
+{
+    private static DefaultFire _instance; //Creating instance of Default Fire
+
+    //If instance already exists, break
+    private DefaultFire()
+    {
+        if (_instance != null)
+        {
+            return;
+        }
+        _instance = this;
+    }
+
+    public static DefaultFire Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                new DefaultFire();
+            }
+            return _instance;
+        }
+    }
+
+    //What to do when state is entered
+    public override void enterState(PlayerShooting _owner)
+    {
+        //_owner.bulletText.text = "Ammo: " + "\u221E";
+    }
+
+    //What to do when state is exited
+    public override void exitState(PlayerShooting _owner)
+    {
+
+    }
+
+    //Update For this state
+    public override void updateState(PlayerShooting _owner)
+    {
+        //_owner.bulletText.text = "Ammo: " + "\u221E";
+        Fire(_owner);
+    }
+
+    //Default Fire Function
+    void Fire(PlayerShooting _owner)
+    {
+        _owner.timeToNextAttack = Time.time + _owner.attackRate;
+        GameObject bullet = Object.Instantiate(_owner.bulletPrefab, _owner.bulletSpawn.position, _owner.bulletSpawn.rotation);
+        bullet.GetComponent<Rigidbody2D>().AddForce(bullet.transform.right * _owner.bulletSpeed);
+        Object.Destroy(bullet, 3f);
+    }
+
+}
---
+++ FinalProject/GameTechFinalProject/Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using StateStuff;
+using UnityEngine.UI;
+
+public class PlayerShooting : MonoBehaviour
+{
+    //Screen Shake Stuff
+    public float shakeLength;
+    public Transform camTransform;
+    [HideInInspector]
+    public float shakeDuration = 0f;
+    public float shakeAmount = 0.7f;
+    public float decreaseFactor = 1.0f;
+    [HideInInspector]
+    public Vector3 originalPos;
+
+    public GameObject bulletPrefab; //Bullet Object
+
+    [HideInInspector]
+    public Transform bulletSpawn; //Where the bullet is spawned from
+
+    public StateMachine<PlayerShooting> stateMachine { get; set; } //Statemachine for different attacks
+
+    [HideInInspector]
+    public int ammoCount = 1000; //Ammo count for weapons
+
+    public Text bulletText; //The ammo count text
+
+    public float AttackSpeed; //Speed the player shoots
+    public float BulletSpeed; //Speed of the bullet
+
+    [HideInInspector]
+    public float attackRate = 0; //Attack timer
+    [HideInInspector]
+    public float timeToNextAttack = 0; //how long until next attack
+
+    [HideInInspector]
+    public float bulletSpeed; //How quickly bullet travels
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalPos = camTransform.position; //Original Camera Position
+        ammoCount = 1000; //Default Ammo count
+        bulletSpeed = BulletSpeed; //Set bullet speed
+        attackRate = AttackSpeed; //Set attack speed
+        stateMachine = new StateMachine<PlayerShooting>(this); //Create Statemachine of type player shooting
+        bulletSpawn = GameObject.Find("BulletSpawn").transform; //Get Bullet spawn location
+        stateMachine.ChangeState(DefaultFire.Instance); //Set the current state
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //If ammo count is 0, go back to default fire
+        if (ammoCount == 0)
+        {
+            stateMachine.ChangeState(DefaultFire.Instance);
+        }
+
+        //If button is pressed, fire
+        if (Input.GetKey(KeyCode.Space) && Time.time > timeToNextAttack)
+        {
+            stateMachine.Update();
+            shakeDuration = shakeLength;
+        }
+        ScreenShake(); //Shake Screen
+    }
+
+    //Screen shake
+    void ScreenShake()
+    {
+        if (shakeDuration > 0) //If shaking is ready
+        {
+            camTransform.localPosition = originalPos + Random.insideUnitSphere * shakeAmount;
+
+            shakeDuration -= Time.deltaTime * decreaseFactor;
+        }
+        else
+        {
+            shakeDuration = 0f;
+            camTransform.localPosition = originalPos;
+        }
+    }
+}
